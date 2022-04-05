d1de8258cbb70a3e6434d2a228c80a2b0ba60151
Khai <khailqd81@gmail.com>
2022-04-05 16:44:29 +0700

    Change port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 var cors = require('cors')
 const router = require('./routers');
 require('dotenv').config();
